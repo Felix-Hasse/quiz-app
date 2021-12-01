17cb116bfb7fc9c7b1309622c06cd1319c755901
Felix Hasse <f.hasse@gmail.com>
2021-12-01 19:39:55 +0100

    ...

---
+++ main.js
@@ -0,0 +1,28 @@
+const quizData = [
+    {
+        question: "What is the capital of India?",
+        a: "New Delhi",
+        a: "Mumbai",
+        a: "Chennai",
+        correct:'a'
+    },{
+        question: "What is the capital of USA?",
+        a:'Washington',
+        b:'New York',
+        c:'California',
+        correct:'a'
+    },{
+        question: "What is the capital of Germany?",
+        a:'Munich',
+        b:'Berlin',
+        c:'Hamburg',
+        correct:'b'       
+    },{
+        question: "What is the capital of France?",
+        a:'lyon',
+        b:'nice',
+        c:'paris',
+        correct:'c'
+
+    }
+]
